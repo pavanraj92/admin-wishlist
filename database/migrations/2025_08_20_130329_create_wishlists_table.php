0ed701d65c6ead3141242310c2f0eadd939480f0
Jahnavi <jahnavi.mathur@dotsquares.com>
2025-08-20 14:59:18 +0530

    db changes

---
+++ database/migrations/2025_08_20_130329_create_wishlists_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('wishlists', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->nullable()->constrained('users')->cascadeOnDelete();
+            // Product (optional)
+            if (Schema::hasTable('products')) {
+                $table->foreignId('product_id')->nullable()
+                      ->constrained('products')
+                      ->cascadeOnDelete();
+            } else {
+                $table->unsignedBigInteger('product_id')->nullable();
+            }
+
+            // Course (optional)
+            if (Schema::hasTable('courses')) {
+                $table->foreignId('course_id')->nullable()
+                      ->constrained('courses')
+                      ->cascadeOnDelete();
+            } else {
+                $table->unsignedBigInteger('course_id')->nullable();
+            }
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('wishlists');
+    }
+};
